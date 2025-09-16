8fe2466eee5bd71ca61c13df6ae39f8a4014d04c
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-09-15 23:55:57 -0300

    Update Bilheteria.java

--- ATIVIDADE EXTRA/src/Bilheteria.java
+++ ATIVIDADE EXTRA/src/Bilheteria.java
@@ -4,30 +4,29 @@ import java.util.Scanner;
 
 public class Bilheteria {
     Scanner sc = new Scanner(System.in);
-    private List<Integer>listaQntIngressos = new ArrayList<>();
+    private List<Integer> listaQntIngressos = new ArrayList<>();
     private List<String> listaTipo = new ArrayList<>();
 
     public void status(Vendas vendas) {
-        System.out.println("deseja conferir os Status de hoje da bilheiteria?");
+        System.out.println("Deseja conferir os Status de hoje da bilheteira?");
         String resposta = sc.nextLine();
 
         if (resposta.equalsIgnoreCase("s")) {
-
             dados(vendas);
 
-            int contInteira = 0, contMeia = 0, contpromossional = 0, gratuito = 0;
+            int contInteira = 0, contMeia = 0, contPromocional = 0, gratuito = 0;
             for (String tipo : listaTipo) {
                 if (tipo.equalsIgnoreCase("Inteira")) {
                     contInteira++;
                 } else if (tipo.equalsIgnoreCase("Meia")) {
-                    contInteira++;
-                } else if (tipo.equalsIgnoreCase("promossional")) {
-                    contpromossional++;
+                    contMeia++;
+                } else if (tipo.equalsIgnoreCase("Promocional")) {
+                    contPromocional++;
                 } else {
                     gratuito++;
                 }
             }
-            System.out.printf("Inteira: %d\nMeia: %d\nPromossional: %d\nEntrada infantiu: %d", contInteira, contMeia, contpromossional, gratuito);
+            System.out.printf("Inteira: %d\nMeia: %d\nPromocional: %d\nEntrada infantil: %d", contInteira, contMeia, contPromocional, gratuito);
 
             int soma = 0;
             for (int i = 0; i < listaQntIngressos.size(); i++) {
@@ -35,40 +34,49 @@ public class Bilheteria {
             }
 
             System.out.printf("\nTotal de ingressos: %d\n", soma);
-        }else{
+        } else {
             System.out.println("Programa encerrado");
         }
-
     }
 
     public void dados(Vendas venda) {
-        this.listaQntIngressos.add(venda.getQuantidadeIngressso());
-        this.listaTipo.add(venda.getTipo());
-
+        if (venda.getQuantidadeIngressso() > 0) {
+            this.listaTipo.add(venda.getTipo());
+            this.listaQntIngressos.add(venda.getQuantidadeIngressso());
+        } else {
+            System.out.println("Tchau");
+        }
     }
+
     public void executar(Scanner sc) {
         Vendas v = new Vendas();
-        System.out.println("digite a quantidade de ingressos: ");
+        System.out.println("Digite a quantidade de ingressos: ");
         v.setQuantidadeIngressso(sc.nextInt());
         sc.nextLine();
-
         int count = 0;
-        double valor = 0;
-
         double valorTotal = 0;
+
         while (v.getQuantidadeIngressso() != count) {
             System.out.println("Digite a idade: ");
             v.setIdade(sc.nextInt());
             sc.nextLine();
-            System.out.println("Digite o tipo do ingresso: ");
+            System.out.println("Digite o tipo do ingresso, só 100 reais :(inteira, meia, gratis, promocional) ");
             v.setTipo(sc.nextLine());
             valorTotal = v.desconto() + valorTotal;
+            System.out.println("Digite a forma de pagamento: (cartao/dinheiro)");
+            String formaPagamento = sc.nextLine();
+
+            if (formaPagamento.equalsIgnoreCase("cartao")) {
+                v.cartao();
+            } else if (formaPagamento.equalsIgnoreCase("dinheiro")) {
+                v.dinheiro();
+            } else {
+                System.out.println("Opção inválida");
+            }
 
             count += 1;
         }
 
         this.status(v);
-
     }
-
 }
