9582fe39101b9f2524bbe6dcb0309eb21ed7f056
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-09-15 23:55:02 -0300

    Update Vendas.java

--- ATIVIDADE EXTRA/src/Vendas.java
+++ ATIVIDADE EXTRA/src/Vendas.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class Vendas {
 
-
-    private double preco = 100.00,valor;
+    Scanner sc = new Scanner(System.in);
+    private double preco = 100.00, valor;
     private int quantidadeIngressso, idade;
     private String tipo;
 
@@ -16,58 +17,57 @@ public class Vendas {
         return tipo;
     }
 
+    public void setIdade(int idade) { this.idade = idade; }
+    public void setQuantidadeIngressso(int quantidadeIngressso) { this.quantidadeIngressso = quantidadeIngressso; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
 
-    public void setIdade(int idade) {this.idade = idade;}
-    public void setQuantidadeIngressso(int quantidadeIngressso) {this.quantidadeIngressso = quantidadeIngressso;}
-    public void setTipo(String tipo) {this.tipo = tipo;}
-
-    public void setValor(double Valor) {
-        valor = Valor;
+    public double getValor() {
+        return valor;
     }
 
     public double desconto() {
-
-        if (tipo == "Inteira") {
-            return 12.00;
-        } else if (tipo == "Meia" ) {
-            return preco / 2;
-        }
-        else if (tipo == "Promocional" ) {
-            return preco * 0.7;
-        }
-        else{
+        if (tipo.equalsIgnoreCase("Inteira")) {
+            return preco = 100.00;
+        } else if (tipo.equalsIgnoreCase("Meia")) {
+            return preco = preco / 2;
+        } else if (tipo.equalsIgnoreCase("Promocional")) {
+            return preco = preco * 0.7;
+        } else {
             return 0.00;
         }
-
     }
+
     public void cartao() {
         String a = "verificando ... ...  compra autorizada";
         String[] b = a.split(" ");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i <= 5; i++) {
             System.out.println(b[i]);
         }
     }
-    public void dinheiro(Scanner sc) {
+
+    public void dinheiro() {
+        System.out.println("Digite o valor pago pelo cliente: ");
+        double pagamento = sc.nextDouble();
+        sc.nextLine();
 
         while (true) {
-            if (valor < preco) {
-                double diferenca = valor - preco;
-                System.out.printf("Valor insuficiente, o valor do ingresso é %f, e faltam %f. Deseja ainda comprar o ingreço?(s/n)", valor, diferenca);
+            if (pagamento < preco) {
+                double diferenca = preco - pagamento;
+                System.out.printf("Valor insuficiente, o valor do ingresso é %.2f, e faltam %.2f. Deseja ainda comprar o ingresso? (s/n)", preco, diferenca);
                 String decisao = sc.nextLine();
-                if (decisao.equals("n")) {
+
+                if (decisao.equalsIgnoreCase("n")) {
+                    valor = 0;
                     break;
+
                 }
-            }
-            else if (valor > preco) {
-                System.out.printf("Seu troco do ingresso %f, bom filme.", valor-preco);
+            } else if (pagamento > preco) {
+                System.out.printf("Seu troco do ingresso: %.2f, bom filme.", pagamento - preco);
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Bom filme");
                 break;
             }
         }
-
     }
-
 }
