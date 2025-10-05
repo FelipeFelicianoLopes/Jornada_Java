2f27cddefae6e402f72d8b48d5203784d83bd7a2
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-10-05 16:01:54 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,15 +1,1 @@
-public class Carro extends Veiculo {
-    private int nPortas;
 
-    public Carro(String placa, String marca, double preco, int nPortas) {
-        super(placa, marca, preco);
-        this.nPortas = nPortas;
-    }
-
-    @Override
-    public void exibir() {
-        super.exibir();
-        System.out.println("Número de portas: " + nPortas);
-    }
-
-}
